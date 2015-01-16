7cb00d904bd872cce3ca6feb58577bf8e652c5c2
Daedelus <badasslikeus@gmail.com>
2015-01-17 00:42:34 +0100

    ACTIVE + AUTEUR

--- TD5-MVC/index.php
+++ TD5-MVC/index.php
@@ -1,6 +1,5 @@
 <?php
-
-$loader = include('vendor/autoload.php');
+$loader = include('vendor\autoload.php');
 $loader->add('', 'src');
 
 $app = new Silex\Application;
@@ -19,7 +18,7 @@ $app['model'] = new Sondage\Model\Model(
 
 // Page d'accueil
 $app->match('/', function() use ($app) {
-    return $app['twig']->render('home.html');
+    return $app['twig']->render('home.html', array('active_page' => 'home'));
 })->bind('home');
 
 // S'inscrire
@@ -27,28 +26,40 @@ $app->match('/signin', function() use ($app) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (!empty($_POST['login']) && !empty($_POST['password'])) {
             if($app['model']->signin($_POST['login'], $_POST['password'])){
-                return $app['twig']->render('home.html', array('success' => 'Inscription reussis'));
+                return $app['twig']->render('home.html', array(
+                    'success' => 'Inscription reussis',
+                    'active_page' => 'signin'));
             } else {
-                return $app['twig']->render('signin.html', array('error' => 'Login déja pris'));
+                return $app['twig']->render('signin.html', array(
+                    'error' => 'Login déja pris',
+                    'active_page' => 'signin'));
             }
         }
     }
-    return $app['twig']->render('signin.html', array());
+    return $app['twig']->render('signin.html', array('active_page' => 'signin'));
 })->bind('signin');
 
 // Se loguer
 $app->match('/login', function() use ($app) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (!empty($_POST['login']) && !empty($_POST['password'])) {
-            if($app['model']->login($_POST['login'], $_POST['password'])){
-                $app['session']->set('user', array('username' => $_POST['login']));
-                return $app['twig']->render('home.html', array('success' => 'Vous etes connecté'));
+            $logged = $app['model']->login($_POST['login'], $_POST['password']);
+            if($logged){
+                $app['session']->set('user', array(
+                    'username' => $logged['id'],
+                    'login' => $_POST['login']));
+                return $app['twig']->render('home.html', array(
+                    'success' => 'Vous etes connecté',
+                    'active_page' => 'home'));
             } else {
-                return $app['twig']->render('login.html', array('error' => 'Login ou mot de passe incorrect'));
+                return $app['twig']->render('login.html', array(
+                    'error' => 'Login ou mot de passe incorrect',
+                    'active_page' => 'login'
+                    ));
             }
         }
     }
-    return $app['twig']->render('login.html', array());
+    return $app['twig']->render('login.html', array('active_page' => 'login'));
 })->bind('login');
 
 // Logout
@@ -58,33 +69,114 @@ $app->match('/logout', function() use ($app) {
         $app['session']->remove('user');
     }
 
-    return $app['twig']->render('home.html', array('success' => 'Vous etes déconnecté')); 
+    return $app['twig']->render('home.html', array(
+        'success' => 'Vous etes déconnecté',
+        'active_page' => 'logout')); 
 })->bind('logout');
 
 // Sondages
 $app->match('/sondage', function() use ($app) {
     return $app['twig']->render('sondages.html', array(
-        'sondages' => $app['model']->getAllSondage()
+        'sondages' => $app['model']->getAllSondage(),
+        'active_page' => 'sondages'
     ));
 })->bind('sondages');
 
-// Le sondage
+/*/ Le sondage
 $app->match('/sondage/{id}', function($id) use ($app) {
     return $app['twig']->render('sondage.html', array('sondage' => $app['model']->getSondage($id), 'warning' => 'Vous devez être identifié pour participer! '));
 })->bind('sondage');
+*/
+// Les reponses des sondage
+// echo "session = > ".$app['session']->get('user')['username'].'<br>';
 
-// Ajouter un sondage
-$app->match('/addsondage', function() use ($app) {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if (!empty($_POST['question']) && !empty($_POST['answer1']) && !empty($_POST['answer2']) && !empty($_POST['answer3'])) {
-            if($app['model']->addsondage($_POST['question'], $_POST['answer1'], $_POST['answer2'], $_POST['answer3'])){
-                return $app['twig']->render('addsondage.html', array('success' => 'Sondage soumis avec succès'));
-            } else {
-                return $app['twig']->render('addsondage.html', array('error' => 'Erreur SQL'));
+$app->match('/sondage/{id}', function($id) use ($app) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        
+        if(!empty($_POST['answer']) && $app['session']->has('user'))
+        {
+            // echo "post activé - ";
+            // echo "session activé - ";
+            $answered = $app['model']->findAnswer($id, $app['session']->get('user')['username']);
+            if ($answered) 
+            {
+                $answers = $app['model']->findAllAnswers($id);
+                $total = $app['model']->totalAnswers($id);
+
+                //var_dump($answers);
+                //var_dump($total);
+                // echo "deja rep";
+                return $app['twig']->render('answers.html', array(
+                    'answers' => $answers,
+                    'total' => $total,
+                    'active_page' => 'sondages'));
+            }
+            else
+            {
+                // echo "pas deja rep";
+                if($app['model']->putAnswer($app['session']->get('user')['username'],$id,$_POST['answer']))
+                {
+                    $answers = $app['model']->findAllAnswers($id);
+                    $total = $app['model']->totalAnswers($id);
+                    return $app['twig']->render('answers.html', array(
+                        'answers' => $answers,
+                        'total' => $total,
+                        'success' => 'Votre reponse à bien été enregistré',
+                    'active_page' => 'sondages'));
+                }
+                return $app['twig']->render('sondage.html', array(
+                    'warning' => 'Une erreur est survenue !',
+                    'active_page' => 'sondages')); 
             }
         }
     }
-    return $app['twig']->render('addsondage.html', array());
+    else if($app['session']->has('user'))
+    {
+        // echo "post desactivé - ";
+        $answered = $app['model']->findAnswer($id, $app['session']->get('user')['username']);
+        // echo $answered;
+        if ($answered) 
+        {
+            $answers = $app['model']->findAllAnswers($id);
+            $total = $app['model']->totalAnswers($id);
+
+            //var_dump($answers);
+            //var_dump($total);
+            // echo "deja rep";
+            return $app['twig']->render('answers.html', array(
+                'answers' => $answers,
+                'total' => $total,
+                'active_page' => 'sondages'));
+        }
+        return $app['twig']->render('sondage.html', array(
+            'sondage' => $app['model']->findAllAnswersId($id),
+            'active_page' => 'sondages'));
+    }
+    return $app['twig']->render('sondage.html', array(
+        'warning' => 'Vous devez être identifié pour participer!',
+        'active_page' => 'sondages')); 
+})->bind('sondage');
+
+
+
+// Ajouter un sondage
+$app->match('/addsondage', function() use ($app) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && $app['session']->has('user')) {
+        if (!empty($_POST['question']) && !empty($_POST['answer1']) && !empty($_POST['answer2']) && !empty($_POST['answer3'])) {
+            if($app['model']->addsondage($_POST['question'], $_POST['answer1'], $_POST['answer2'], $_POST['answer3'], $app['session']->get('user')['login'])){
+                return $app['twig']->render('addsondage.html', array(
+                    'success' => 'Sondage soumis avec succès',
+                    'active_page' => 'addsondages'));
+            } else {
+                return $app['twig']->render('addsondage.html', array(
+                    'error' => 'Erreur SQL',
+                    'active_page' => 'addsondages'));
+            }
+        }
+    }
+    return $app['twig']->render('addsondage.html', array(
+                    'active_page' => 'addsondages'));
 })->bind('addsondage');
 
 // Fait remonter les erreurs
--- TD5-MVC/src/sondage/Model/Model.php
+++ TD5-MVC/src/sondage/Model/Model.php
@@ -59,7 +59,7 @@ class Model
                 $user = $query->fetch();
                 if ($user['password'] == md5($pw)) {
                     $currentUser = $user;
-                    return true;
+                    return $currentUser;
                 }
             }
         }
@@ -82,17 +82,120 @@ class Model
         return false;
     }
 
-    public function addsondage($q,$a1,$a2,$a3)
+    public function addsondage($q,$a1,$a2,$a3,$auteur)
     {
-        if (!empty($q) && !empty($a1) && !empty($a2) && isset($a3)) {
-            $req = $this->pdo->prepare("INSERT INTO polls (question,answer1,answer2,answer3) VALUES (:q,:asw1,:asw2,:asw3)");
+        if (!empty($q) && !empty($a1) && !empty($a2) && !empty($a3) && !empty($auteur)) {
+            $req = $this->pdo->prepare("INSERT INTO polls (question,answer1,answer2,answer3,auteur) VALUES (:q,:asw1,:asw2,:asw3,:auteur)");
             $req->bindParam(':q',$q);
             $req->bindParam(':asw1',$a1);
             $req->bindParam(':asw2',$a2);
             $req->bindParam(':asw3',$a3);
+            $req->bindParam(':auteur',$auteur);
             $req->execute();
             return true;
         }
         return false;
     }
+
+    public function addAnswer($q,$a1,$a2,$a3)
+    {
+        if (!empty($idu) && !empty($idq) && !empty($asw)) {
+            $req = $this->pdo->prepare("INSERT INTO answers (user_id, poll_id, answer) VALUES (:idu,:idq,:asw)");
+            $req->bindParam(':idu',$idu);
+            $req->bindParam(':idq',$idq);
+            $req->bindParam(':asw',$asw);
+            $req->execute();
+            return true;
+        }
+        return false;
+    }
+
+    public function findAnswer($id_question, $id_user){
+        if (!empty($id_question) && !empty($id_user)) {
+            $req = $this->pdo->prepare("SELECT count(*) as nb FROM answers WHERE user_id=:idu AND poll_id=:idq");
+            $req->bindParam(':idq',$id_question);
+            $req->bindParam(':idu',$id_user);
+            $req->execute();
+            $cursor = $req->fetch();
+
+            if($cursor['nb'] > 0)
+                return true;
+        }
+        return false;
+    }
+
+    public function findAllAnswersId($id_question){
+        if (!empty($id_question)) {
+            $req = $this->pdo->prepare("SELECT * FROM polls WHERE id=:idq");
+            $req->bindParam(':idq',$id_question);
+            $req->execute();
+            $cursor = $req->fetch();
+            return $cursor;
+        }
+        return false;
+    }
+
+    public function findAllAnswers($id_question){
+        if (!empty($id_question)) {
+            $result = array();
+
+            $sql = "SELECT polls.id, polls.question, polls.answer1, polls.answer2, polls.answer3, polls.auteur, count(answer) as nb FROM answers INNER JOIN polls ON answers.poll_id=polls.id WHERE poll_id=:idq GROUP BY answer";
+            $req = $this->pdo->prepare($sql);
+            $req->bindParam(':idq',$id_question);
+            $req->execute();
+            $cursor = $req->fetch();
+
+            //var_dump($cursor);
+
+            $result['id'] = $cursor['id'];
+            $result['question'] = $cursor['question'];
+            $result['answer1'] = $cursor['answer1'];
+            $result['answer2'] = $cursor['answer2'];
+            $result['answer3'] = $cursor['answer3'];
+            $result['auteur'] = $cursor['auteur'];
+
+            $sql = "SELECT count(answer) as nb FROM answers WHERE poll_id=:idq GROUP BY answer";
+            $req = $this->pdo->prepare($sql);
+            $req->bindParam(':idq',$id_question);
+            $req->execute();
+            $cursor = $req->fetchAll();
+
+            
+            if(isset($cursor[0]['nb']))
+                $result['nb1'] = $cursor[0]['nb'];
+            if(isset($cursor[1]['nb']))
+                $result['nb2'] = $cursor[1]['nb'];
+            if(isset($cursor[2]['nb']))
+                $result['nb3'] = $cursor[2]['nb'];
+
+            //var_dump($result);
+
+            return $result;
+        }
+        return false;
+    }
+
+    public function totalAnswers($id_question){
+        if (!empty($id_question)) {
+            $req = $this->pdo->prepare("SELECT count(answer) as nb FROM answers WHERE poll_id=:idq");
+            $req->bindParam(':idq',$id_question);
+            $req->execute();
+            $cursor = $req->fetch();
+            return $cursor;
+        }
+        return false;
+    }
+
+    public function putAnswer($id_user,$id_question,$id_answer){
+        if (!empty($id_user) && !empty($id_question) && !empty($id_answer)){
+            $sql = "INSERT INTO answers (user_id,poll_id,answer) VALUES (:idu,:idq,:ida)";
+            $req = $this->pdo->prepare($sql);
+            $req->bindParam(':idu',$id_user);
+            $req->bindParam(':idq',$id_question);
+            $req->bindParam(':ida',$id_answer);
+            return $req->execute();
+        }
+        return false;
+    }
+
 }
